1a70c61c68221c5099714e26623691031d52db0d
andre <andre.cainda.524@outlook.pt>
2025-07-17 21:11:53 +0100

    SeedDb

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -29,6 +29,8 @@ namespace SuperShop
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddTransient<SeedDb>();
+           
             services.AddControllersWithViews();
         }
 

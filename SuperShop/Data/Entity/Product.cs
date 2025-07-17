b55d11e9f11c6c9fe5f59661b52e56db2085684c
andre <andre.cainda.524@outlook.pt>
2025-07-17 20:38:09 +0100

    Modify Products

--- SuperShop/Data/Entity/Product.cs
+++ SuperShop/Data/Entity/Product.cs
@@ -7,6 +7,8 @@ namespace SuperShop.Data.Entity
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length. ")]
         public string Name { get; set; }
 
 
@@ -18,11 +20,11 @@ namespace SuperShop.Data.Entity
         public string imageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase {  get; set; }
+        public DateTime? LastPurchase {  get; set; }
 
 
         [Display(Name = "Last")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
